1b27e0f5bf1efd4548002c9c36ae11447b3b6052
hal468a <asdfg880226@gmail.com>
2022-07-14 16:05:17 +0800

    change let to var

--- ios/DoorController/ViewController.swift
+++ ios/DoorController/ViewController.swift
@@ -28,28 +28,33 @@ class ViewController: UIViewController {
         self.performSegue(withIdentifier: "segue_controller", sender: self)
     }
     
+    //按下Up按鈕時，傳送"Up"的get request至開發板
     @IBAction func Up(_ sender: Any) {
         sendRequest(address: "http://192.168.0.73/Up")
         Console.text = "Up"
         
     }
     
+    //按下Stop按鈕時，傳送"Stop"的get request至開發板
     @IBAction func Stop(_ sender: Any) {
         sendRequest(address: "http://192.168.0.73/Stop")
         Console.text = "Stop"
         
     }
     
+    //按下Lock按鈕時，傳送"Lock"的get request至開發板
     @IBAction func Lock(_ sender: Any) {
         sendRequest(address: "http://192.168.0.73/Locked")
         Console.text = "Locked"
     }
     
+    //按下Down按鈕時，傳送"Down"的get request至開發板
     @IBAction func Down(_ sender: Any) {
         sendRequest(address: "http://192.168.0.73/Down")
         Console.text = "Down"
     }
     
+    //sendRequest function
     func sendRequest(address: String){
         var url = NSURL(string:address)
         var urlreq = NSURLRequest(url: url! as URL)
